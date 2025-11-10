757eeebe8a904f09488510b11704f383e4ee4cd3
YOUR_NEW_NAME <vishnu.g.sur@gmail.com>
2025-11-10 11:30:46 -0800

    feat: add search_queries to reflection_node

--- core/helpers/nodes.py
+++ core/helpers/nodes.py
@@ -273,6 +273,7 @@ def reflection_node(state: ResearchState) -> ResearchState:
     return {
         "messages": state["messages"],
         "research_brief": research_brief,
+        "search_queries": response.follow_up_queries if response.needs_more_context else [],
         "search_results": search_results,
         "compressed_findings": response.compressed_findings,
         "knowledge_gaps": response.knowledge_gaps,
